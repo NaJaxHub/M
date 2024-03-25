3c52307f92a87e2b18a8426d43f1a783809519f0
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2024-03-25 16:52:34 +0700

    Update M.lua

--- M.lua
+++ M.lua
@@ -11040,12 +11040,12 @@ spawn(function()
 				CheckQuest()
 				for i , v in pairs(game:GetService("Workspace")._WorldOrigin.EnemySpawns:GetChildren()) do
 					if string.find(v.Name, MobName) then
-						repeat task.wait()
+						--repeat task.wait()
 							--if (v.CFrame.Position - game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position).Magnitude >= 50 then
 								_G.CFrameAutoFarmPos = v.CFrame * CFrame.new(0,89,0)
 							--end
 							task.wait(.15)
-						until not _G.Auto_Farm_Level
+						--until not _G.Auto_Farm_Level
 					end
 				end
 			end)
@@ -11104,11 +11104,14 @@ v.Head.CanCollide = false v.Humanoid.WalkSpeed = 0 v.HumanoidRootPart.CanCollide
 											if game.Players.LocalPlayer.Character:FindFirstChild("Black Leg") and game.Players.LocalPlayer.Character:FindFirstChild("Black Leg").Level.Value >= 150 then
 												game:service("VirtualInputManager"):SendKeyEvent(true, "V", false, game) game:service("VirtualInputManager"):SendKeyEvent(false, "V", false, game)
 											end
+											if not game:GetService("Workspace").Enemies:FindFirstChild(MobName) and not v:FindFirstChild("Humanoid")  then
+												Tween(_G.CFrameAutoFarmPos)
+											end
 										until not _G.Auto_Farm_Level or v.Humanoid.Health <= 0 or QuestC.Visible == false
 									end
 								end
 							end
-						else _G.MrMaxNaJaPosMon = true
+						else --_G.MrMaxNaJaPosMon = true
 							Tween(_G.CFrameAutoFarmPos)
 							UnEquipWeapon(_G.Select_Weapon)
 							--_G.SuperFastAttack = false
